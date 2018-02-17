65bc2257ad1d81187523be01fa13eb4116269fc0
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2018-02-17 20:30:59 +0400

    page 103

---
+++ lessons/HeadFirst_Python/vowels3.py
@@ -0,0 +1,9 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("Provide a world to search for vowels: ")
+found = []
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+for vowels in found:
+    print(vowels)
