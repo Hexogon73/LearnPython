706e2811795b75802f18e90f6eb76823a06f68bb
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2018-05-02 17:37:07 +0400

    156

---
+++ lessons/HeadFirst_Python/Chapter 2 Collections/vowels5.py
@@ -0,0 +1,12 @@
+"""page 148"""
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("Provide a world to search for vowels: ")
+
+found = {}
+
+for letter in word:
+    if letter in vowels:
+        found[letter] += 1
+
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s).')
