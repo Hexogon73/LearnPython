314cb59b5cf02b07da99c1b4d0784039dbf245d0
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2019-05-26 19:30:56 +0400

    added singleton pattern example

---
+++ lessons/patterns/singleton.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class Singleton(object):
+    obj = None  # единственный экземпляр класса
+
+    def __new__(cls, *args, **kwargs):
+        if cls.obj is None:
+            cls.obj = object.__new__(cls, *args, **kwargs)
+        return cls.obj
+
+
+single = Singleton()
+single.attr = 42
+newSingle = Singleton()
+print(newSingle.attr)  # 42
+print(newSingle is single)  # true
