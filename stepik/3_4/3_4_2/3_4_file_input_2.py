29bfce4612faa3faff5e45ab958e6f1edb22f153
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2019-11-04 01:15:07 +0400

    Stepik: added file_input 3_4_2, 3_4_3

---
+++ stepik/3_4/3_4_2/3_4_file_input_2.py
@@ -0,0 +1,25 @@
+# https://stepik.org/lesson/3363/step/2?unit=1135
+
+import os
+import re
+
+file_path = os.path.join('.', 'dataset_3363_2.txt')
+text = None
+with open(file_path, 'r') as f:
+    text = f.readline()
+
+numbers = re.findall(r'\d+', text)
+repl = re.sub(r'\d+', ',<num>,', text)
+splited = re.split(r',', repl)
+
+previous = ''
+current = ''
+result = ''
+for idx, c in enumerate(splited):
+    previous = current
+    current = c
+    if current == '<num>':
+        num = int(numbers.pop(0))
+        splited[idx] = num
+        result += previous * num
+print(result)
---
+++ stepik/3_4/3_4_3/3_4_file_input_3.py
@@ -0,0 +1,27 @@
+# https://stepik.org/lesson/3363/step/3?unit=1135
+
+import os
+
+file_path = os.path.join('.', 'dataset_3363_3.txt')
+text = ''
+with open(file_path, 'r') as f:
+    all_text = f.readlines()
+    for s in all_text:
+        text += s.lower()
+
+splited = text.split()
+print(splited)
+
+count = 0
+count_list = {}
+for s in splited:
+    count_list[s] = splited.count(s)
+print(count_list)
+
+max_k = ''
+max_v = 0
+for k, v in count_list.items():
+    if v > max_v:
+        max_v = v
+        max_k = k
+print(max_k, max_v)
