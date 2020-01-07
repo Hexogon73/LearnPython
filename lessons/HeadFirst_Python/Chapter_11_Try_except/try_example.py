7d25a0aa69cb96c9a6484bfcb8318d7c5392890f
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2020-01-07 17:55:27 +0400

    HF_Python: append Chapter_11_Try_except, web_app: use try..except

---
+++ lessons/HeadFirst_Python/Chapter_11_Try_except/try_example.py
@@ -0,0 +1,11 @@
+"""page 459..464"""
+try:
+    with open('myfile.txt') as fh:
+        file_data = fh.read()
+    print(file_data)
+except FileNotFoundError:
+    print('The data file is missing')
+except PermissionError:
+    print('This is not allowed')
+except Exception as err:
+    print('Some other error occurred', str(err))
